e4b0bd83f2c36a43b43702d98c2390d581f85c93
birendraetldev <birendraetldev@gmail.com>
2020-05-27 20:32:38 +0530

    maven_first_code1.0

---
+++ src/main/scala/maven_build_proj/maven_test_code.scala
@@ -0,0 +1,13 @@
+package maven_build_proj
+import org.apache.spark.sql.SparkSession
+object maven_test_code {
+  def main(args: Array[String]): Unit =
+    {
+      val spark = SparkSession.builder().master("local").appName("DataFrame").getOrCreate()
+      val dff = spark.read.csv("C:/Birendra/Scala_Spark_src/test.csv")
+      dff.show()
+      val df = spark.read.format("csv").option("header", "true").load("C:/Birendra/Scala_Spark_src/test.csv")
+      df.show()
+      df.select("Nmae", "Age").show()
+    }
+}
